3379d3cb59a543fd131e117389217fd49c813a55
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-17 11:24:27 +0200

    enable crossorigin

--- src/main/java/dev/controller/CollegueController.java
+++ src/main/java/dev/controller/CollegueController.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import dev.entity.Collegue;
 import dev.Service.CollegueService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin //permet de communiquer inter server
 @RestController
 @RequestMapping("/collegues")
 public class CollegueController {
